09e975ae53c3f81b78d49f75416bf08edf649cd0
Arkaitz <me@arkaitz.dev>
2023-05-11 03:37:24 +0200

    Added README.md file, and performed required changes to Cargo.toml in order to publishing to crates.io .

---
+++ README.md
@@ -0,0 +1,11 @@
+### Spa server
+
+This is a lightweight web server entirely writen in Rust, using Rocket framework.
+
+## Install
+
+If you have Rust installed, at least v1.69, just run the following command:
+
+cargo install spaserver
+
+Please, refer to crates.io for further information.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
+//! ### Spaserver
+//! 
+//! As the name suggests, it's a lightweight web server aimed at serving SPA web pages.
+//! It simply performs a redirection of any URL (relative to your domain, of course) to
+//! "/#/whateveryoutype". By doing this, if you properly process the new URL in your routes,
+//! you'll be able to correctly deal with hand written URLs.
+//! 
+//! In order to give you just a little bit of freedom, you can set the following environment
+//! variables:
+//! 
+//! * ROOT_DIR: It defaults to "public". It's where your index.html file lives, relative to 
+//! where spaserver is executed, and the base of other folders you need for your SPA.
+//! 
+//! * ASSETS_DIR: It defaults to "assets". It's where you can find style files, icons and other
+//! stuff.
+//! 
+//! * API_URL: In case you use an external API (for example for database lookups, or 
+//! authentication, etc), every time you call "/api", it will be rewritten to this URL. Defaults
+//! to the "standard" rewrite policy previously explained.
+//! 
+//! As you can see, this crate it's not intended for any complex configuration. Of course, it
+//! comes with absolutely no guarantee, so use it at your own risk.
+
 #[macro_use]
 extern crate rocket;
 use rocket::fs::FileServer;
 use rocket::response::Redirect;
 use std::env;
 use std::path::PathBuf;
-
-// #[get("/")]
-// async fn index() -> Option<NamedFile> {
-//     NamedFile::open("static/index.html").await.ok()
-// }
 
 #[get("/<path..>", rank = 3)]
 fn rewrite(path: PathBuf) -> Redirect {
@@ -19,7 +37,7 @@ fn rewrite(path: PathBuf) -> Redirect {
 fn api_get(path: PathBuf) -> Redirect {
     let path_str: String = path.to_string_lossy().to_string();
     match env::var("API_URL") {
-        Ok(api_url) => Redirect::permanent(format!("{}{}", api_url, path_str)),
+        Ok(api_url) => Redirect::permanent(format!("{}/{}", api_url, path_str)),
         _ => Redirect::permanent(format!("/#/{}", path_str)),
     }
 }
@@ -27,9 +45,11 @@ fn api_get(path: PathBuf) -> Redirect {
 #[launch]
 fn rocket() -> _ {
     let root = env::var("ROOT_DIR").unwrap_or("public".to_string());
+    let assets = env::var("ASSETS_DIR").unwrap_or("assets".to_string());
+
     rocket::build()
         .mount("/", FileServer::from(&root).rank(2))
-        .mount("/assets", FileServer::from(format!("{}/assets", root)).rank(1))
+        .mount(format!("/{}",assets), FileServer::from(format!("{}/{}", root, assets)).rank(1))
         .mount("/", routes![rewrite])
         .mount("/api", routes![api_get])
 }
